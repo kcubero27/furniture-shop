8bc249c13ee186a564f79d39853c5bcf80e86556
Karen Cubero <kaaerreeene@gmail.com>
2019-08-03 21:45:44 +0200

    core(dependencies): update dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,50 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState } from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="https://tailwindcss.com//img/card-top.jpg" alt="Sunset in the mountains" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
-            eaque, exercitationem praesentium nihil.
-          </p>
-        </div>
-        <div className="px-6 py-4">
-          <span
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-          <span
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-          <span
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-        </div>
-    </div>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const defaultItems = 5
+
+const IndexPage = () => {
+  const [items, setItems] = useState(5)
+
+  const getItems = () => {
+    let images = [];
+
+    for (let i = 0; i < items; i++) {
+      images.push(
+        <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="https://picsum.photos/id/938/200/300" alt="Sunset in the mountains"/>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+              eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+        <span
+          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>,
+      )
+    }
+
+    return images;
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home"/>
+      {getItems()}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setItems(items + defaultItems)}>
+        Button
+      </button>
+    </Layout>
+  )
+}
 
 export default IndexPage
